f56a61d9bd8c6f923ff900f71e516163ba90a69e
zubairAhmed777 <findzubair777@gmail.com>
2025-09-21 10:10:00 +0100

    unit-test using github actions

---
+++ test_main.py
@@ -0,0 +1,8 @@
+from main import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    
+    assert response.status_code == 200
+    assert response.get_json() == {'message': 'Hello Github Actions'} 
